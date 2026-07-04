03f44e9bd087177d70b79df7b7327bdabcba0adf
yousef-abdelmagid <yousef.abdelmagid@inrs.ca>
2026-07-03 20:55:36 -0400

    small comment on 3M0X dataPrep script

--- code/dataPrep/main.py
+++ code/dataPrep/main.py
@@ -1,6 +1,29 @@
 import os
 from prepare_3M0X import prepare_3M0X
 
+# Extract 3M0X.tar.gz from OneDrive to data_folder then run this file
+# Tree shoule be as follows, where wav contains the audio files
+#
+# 3M0X
+# ├── en
+# │   ├── attemptedClones.txt
+# │   ├── attemptedSpeech.txt
+# │   ├── voices_2026-06-20_09-23-17.csv
+# │   └── wav/
+# ├── fr
+# │   ├── attemptedClones.txt
+# │   ├── attemptedSpeech.txt
+# │   ├── genSpeechLogs.txt
+# │   ├── voices_2026-06-20_09-47-35.csv
+# │   └── wav/
+# └── pt
+#     ├── attemptedClones.txt
+#     ├── attemptedSpeech.txt
+#     ├── genSpeechLogs.txt
+#     ├── voices_2026-06-29_17-37-11.csv
+#     └── wav/
+#
+
 SCRATCH = os.environ["SCRATCH"]
 language = "en"
 
